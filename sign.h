4e96242fbddad9500a1bf1aa323b323ad47631d9
Duc Nguyen <ductri.nguyen@sandboxquantum.com>
2023-04-27 19:02:12 +0000

    clean up and add namespace for sign

--- sign.h
+++ sign.h
@@ -15,7 +15,7 @@
  *
  * Returns 0 (success), -1 otherwise
  **************************************************/
-#define crypto_sign_keypair XMSS_NAMESPACE(keypair)
+#define crypto_sign_keypair XMSS_NAMESPACE(crypto_sign_keypair)
 int crypto_sign_keypair(unsigned char *pk, unsigned char *sk);
 
 /*************************************************
@@ -31,7 +31,7 @@ int crypto_sign_keypair(unsigned char *pk, unsigned char *sk);
  *
  * Returns 0 (success), -1 otherwise
  **************************************************/
-#define crypto_sign XMSS_NAMESPACE(sign)
+#define crypto_sign XMSS_NAMESPACE(crypto_sign)
 int crypto_sign(unsigned char *sm, unsigned long long *smlen,
                 const unsigned char *m, unsigned long long mlen, unsigned char *sk);
 
@@ -50,7 +50,7 @@ int crypto_sign(unsigned char *sm, unsigned long long *smlen,
  *
  * Returns 0 if signed message could be verified correctly and -1 otherwise
  **************************************************/
-#define crypto_sign_open XMSS_NAMESPACE(verify)
+#define crypto_sign_open XMSS_NAMESPACE(crypto_sign_open)
 int crypto_sign_open(unsigned char *m, unsigned long long *mlen,
                      const unsigned char *sm, unsigned long long smlen, const unsigned char *pk);
 
@@ -64,7 +64,7 @@ int crypto_sign_open(unsigned char *m, unsigned long long *mlen,
  *
  * Returns 0 (sucess), -1 otherwise
  **************************************************/
-#define crypto_remaining_signatures XMSS_NAMESPACE(remaining_signatures)
+#define crypto_remaining_signatures XMSS_NAMESPACE(crypto_remaining_signatures)
 int crypto_remaining_signatures(unsigned long long *remain, const unsigned char *sk);
 
 #endif 
