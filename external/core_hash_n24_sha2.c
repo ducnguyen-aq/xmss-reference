039fd0c8e446336fe2b058c044493545a1bb3f73
Duc Nguyen <ductri.nguyen@sandboxquantum.com>
2023-05-15 17:45:53 +0000

    optimize core_hash build

---
+++ external/core_hash_n24_sha2.c
@@ -0,0 +1,14 @@
+#include <oqs/sha2.h>
+
+#include "hash.h"
+
+int core_hash(const xmss_params *params,
+              unsigned char *out,
+              const unsigned char *in, unsigned long long inlen)
+{
+    (void)params;
+    unsigned char buf[32];
+    OQS_SHA2_sha256(buf, in, inlen);
+    memcpy(out, buf, 24);
+    return 0;
+}
