95b1613aec547614f90b55699315bff1ca93d0dd
Duc Nguyen <ductri.nguyen@sandboxquantum.com>
2023-07-16 15:55:01 -0400

    pretty print

--- external/test/print_keysize_signature_size.c
+++ external/test/print_keysize_signature_size.c
@@ -5,21 +5,21 @@
 int main(void) {
 	const char *params[] = {
 		"XMSS-SHA2_10_256",
-		"XMSS-SHA2_10_512",
-		"XMSS-SHAKE_10_256",
-		"XMSS-SHAKE_10_512",
 		"XMSS-SHA2_16_256",
-		"XMSS-SHA2_16_512",
-		"XMSS-SHAKE_16_256",
-		"XMSS-SHAKE_16_512",
 		"XMSS-SHA2_20_256",
-		"XMSS-SHA2_20_512",
+		"XMSS-SHAKE_10_256",
+		"XMSS-SHAKE_16_256",
 		"XMSS-SHAKE_20_256",
+		"XMSS-SHA2_10_512",
+		"XMSS-SHA2_16_512",
+		"XMSS-SHA2_20_512",
+		"XMSS-SHAKE_10_512",
+		"XMSS-SHAKE_16_512",
 		"XMSS-SHAKE_20_512",
 	};
 	uint32_t oid;
 	xmss_params xmss;
-	printf("| Algorithm | oid | sk | pk | sig\n");
+	printf("| Algorithm | oid | sk | pk | sig | n\n");
 	for (int i = 0; i < 12; i++) {
 		if (xmss_str_to_oid(&oid, params[i])) {
 			printf("xmss_str_to_oid failed\n");
@@ -31,8 +31,8 @@ int main(void) {
 			return -1;
 		}
 
-		printf("| %s \t| 0x%02x | %4llu | %3u | %4u\n", params[i], oid, xmss.sk_bytes,
-		       xmss.pk_bytes, xmss.sig_bytes);
+		printf("| %s \t\t| 0x%02x | %5llu | %3u | %5u | %2u\n", params[i], oid, xmss.sk_bytes,
+		       xmss.pk_bytes, xmss.sig_bytes, xmss.n);
 	}
 
 	const char *params_mt[] = {
@@ -44,10 +44,18 @@ int main(void) {
 		"XMSSMT-SHA2_60/3_256",
 		"XMSSMT-SHA2_60/6_256",
 		"XMSSMT-SHA2_60/12_256",
+		"XMSSMT-SHAKE_20/2_256",
+		"XMSSMT-SHAKE_20/4_256",
+		"XMSSMT-SHAKE_40/2_256",
+		"XMSSMT-SHAKE_40/4_256",
+		"XMSSMT-SHAKE_40/8_256",
+		"XMSSMT-SHAKE_60/3_256",
+		"XMSSMT-SHAKE_60/6_256",
+		"XMSSMT-SHAKE_60/12_256",
 	};
 	uint32_t oid_mt;
 
-	for (int i = 0; i < 8; i++) {
+	for (int i = 0; i < 16; i++) {
 		if (xmssmt_str_to_oid(&oid_mt, params_mt[i])) {
 			printf("xmssmt_str_to_oid failed\n");
 			return -1;
@@ -58,8 +66,8 @@ int main(void) {
 			return -1;
 		}
 
-		printf("| %s \t| 0x%02x | %5llu | %3u | %4u\n", params_mt[i], oid_mt, xmss.sk_bytes,
-		       xmss.pk_bytes, xmss.sig_bytes);
+		printf("| %s  \t| 0x%02x | %5llu | %3u | %5u | %2u\n", params_mt[i], oid_mt, xmss.sk_bytes,
+		       xmss.pk_bytes, xmss.sig_bytes, xmss.n);
 	}
 	return 0;
 }
