ef262fb7b308664573d7d81f400dd2e5bf451562
Duc Nguyen <106774416+ducnguyen-sb@users.noreply.github.com>
2023-07-16 19:54:06 -0400

    Refactor for Fast Option only (#20)
    * use make info to print sk, pk, sig and oid
    
    * update gitignore
    
    * add liboqs_params table
    
    * refactor sign_params.h
    
    * revert core_hash for easy build
    
    * adapt to only fast parameters
    
    * move to test/
    
    * newline to remove warning
    
    * remove for new test
    
    * remove warning
    
    * add bash script to generate all KATs files
    
    * proper clean up makefile
    
    * remove define
    
    * for speed, only iterate 1
    
    * pretty print
    
    * fix typo
    
    * add xmssmt for shake128
    
    * add KATs
    
    * remove newline
    
    * use newline
    
    * add xmssmt shake128

---
+++ external/gen_all_kat.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Only run on MAC
+cmd="clang -Wall -O3 -Wextra -Wpedantic  -o test/sign_gen_NIST_KAT_fast params.c hash.c hash_address.c wots.c xmss.c xmss_core_fast.c xmss_commons.c utils.c test/sign_gen_NIST_KAT.c sign.c -L/Users/ductri.nguyen/Work/openssl/oqs/lib -I/Users/ductri.nguyen/Work/openssl/oqs/include/ -L/opt/homebrew/opt/openssl@1.1/lib -I/opt/homebrew/opt/openssl@1.1/include -lcrypto -loqs"
+
+echo "XMSSMT=0"
+for i in $(jot -w 0x%x 12 1);
+do 
+    echo "-DXMSS_OID_INT=${i}"
+    new_cmd="${cmd} -DXMSSMT=0 -DXMSS_OID_INT=${i}"
+    # Compile
+    eval ${new_cmd}
+    # Execute
+    time test/sign_gen_NIST_KAT_fast
+done
+
+echo "XMSSMT=1"
+for i in $(jot -w 0x%x 8 1);
+do 
+    echo "-DXMSS_OID_INT=${i}"
+    new_cmd="${cmd} -DXMSSMT=1 -DXMSS_OID_INT=${i}"
+    # Compile
+    eval ${new_cmd}
+    # Execute
+    time test/sign_gen_NIST_KAT_fast
+done
+
+echo "XMSSMT=1"
+for i in $(jot -w 0x%x 8 17);
+do 
+    echo "-DXMSS_OID_INT=${i}"
+    new_cmd="${cmd} -DXMSSMT=1 -DXMSS_OID_INT=${i}"
+    # Compile
+    eval ${new_cmd}
+    # Execute
+    time test/sign_gen_NIST_KAT_fast
+done
